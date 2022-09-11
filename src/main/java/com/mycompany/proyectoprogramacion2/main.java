066226f7e796f7570b153c34c35f386338a408b6
Wessly Zelaya <wess_zelaya@hotmail.com>
2022-09-10 22:57:35 -0600

    eliminando residuos

--- src/main/java/com/mycompany/proyectoprogramacion2/main.java
+++ src/main/java/com/mycompany/proyectoprogramacion2/main.java
@@ -15,19 +15,11 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-<<<<<<< HEAD
-cambiarContraseña cambio = new cambiarContraseña();
-cambio.setVisible(true);
 
+        cambiarContraseña cambio = new cambiarContraseña();
+        cambio.setVisible(true);
 
-=======
-        
-        
-        
-        
         ///Prueba de aporte a repositorio
-        
->>>>>>> 29a3352ed43810e8a50521285339ac9dd9bb29ca
     }
-    
+
 }
