a7f8e3446f3fbf2b3ae6e8880cfefa05c024660d
HectorMuri <hecmur18@gmail.com>
2022-09-10 21:41:42 -0600

    Prueba de commit y aporte a repositorio

--- src/main/java/com/mycompany/proyectoprogramacion2/main.java
+++ src/main/java/com/mycompany/proyectoprogramacion2/main.java
@@ -15,6 +15,12 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        
+        
+        ///Prueba de aporte a repositorio
+        
     }
     
 }
